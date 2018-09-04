b90c87577f33beb83e39f05df5a74b4120559f0d
Prayugo Dwi <Dwi.deni24@gmail.com>
2018-09-04 21:42:44 +0700

    add config jwt and api user,content
    add config jwt and api user,content

--- application/controllers/Api/Users.php
+++ application/controllers/Api/Users.php
@@ -1,13 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class AppendIterator extends CI_Controller {
+class users extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
 		$this->load->helper('form');
     $this->load->library('session');
+		$this->load->library('Authorization_Token');
 		$this->load->model('cek_login');
 
 	}
