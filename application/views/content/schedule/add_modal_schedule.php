af18606c62795e9950c0d709a9906214056f13bc
Arya Wicaksana <wicaksana94@gmail.com>
2019-02-24 22:07:53 +0700

    finish add schedule modal + input swal validation

--- application/views/content/schedule/add_modal_schedule.php
+++ application/views/content/schedule/add_modal_schedule.php
@@ -2,8 +2,84 @@
   div.modal-body {
     padding-top: 0px !important;
   }
-</style>
 
+    .swal-footer {
+        display: flex !important;
+        justify-content: center !important;
+    }
+</style>
+<script>
+function submit_data() {
+  var schedule_id = $( "#schedule_id" ).val();
+  var start_time = $( "#start_time" ).val();
+  var finish_time = $( "#finish_time" ).val();
+  var day = $( "#day" ).val();
+  var jam_mulai = $( "#jam_mulai" ).val();
+  var jam_akhir = $( "#jam_akhir" ).val();
+  var guru_id = $( "#guru_id" ).val();
+  var mapel_id = $( "#mapel_id" ).val();
+  var room_id = $( "#room_id" ).val();
+  var note = $( "#note" ).val();
+  var kelas = $( "#kelas" ).val();
+  var jurusan = $( "#jurusan" ).val();
+  var subkelas = $( "#subkelas" ).val();
+  var kelas_jurusan_subkelas = kelas+"_"+jurusan+"_"+subkelas;
+  if ($( "#start_time" ).val() == '' ) {
+    swal("Start Time Harus Diisi", "Isilah pukul berapa dimulainya pelajaran" , "info");
+  } else if ($( "#finish_time" ).val() == '' ) {
+    swal("Finish Time Harus Diisi", "Isilah pukul berapa selesainya pelajaran" , "info");
+  } else if ($( "#jam_mulai" ).val() == '' ) {
+    swal("Jam Mulai Harus Diisi", "Isilah jam ke- berapa dimulainya pelajaran" , "info");
+  } else if ($( "#jam_akhir" ).val() == '' ) {
+    swal("Jam Akhir Harus Diisi", "Isilah jam ke- berapa selesainya pelajaran" , "info");
+  } else if ($( "#guru_id" ).val() == '' ) {
+    swal("Data Guru Harus Diisi", "Isilah data guru yang mengajar" , "info");
+  } else if ($( "#mapel_id" ).val() == '' ) {
+    swal("Data Mapel Harus Diisi", "Isilah data mata pelajaran" , "info");
+  } else if ($( "#room_id" ).val() == '' ) {
+    swal("Data Ruangan Harus Diisi", "Isilah data ruangan" , "info");
+  } else {
+     $.ajax({
+      type: 'POST',
+      url: '<?php echo base_url()?>Schedule/do_insert',
+      data: 
+          {
+            schedule_id: schedule_id,
+            start_time: start_time,
+            finish_time: finish_time,
+            day: day,
+            jam_mulai: jam_mulai,
+            jam_akhir: jam_akhir,
+            guru_id: guru_id,
+            mapel_id: mapel_id,
+            room_id: room_id,
+            note: note,
+            kelas: kelas,
+            jurusan: jurusan,
+            subkelas: subkelas
+          },
+      dataType: 'json',
+      success: function(d){
+       // alert(d.status); //will alert ok
+      if (d.status === "ok") {
+            swal("Data Tersimpan", "Terimakasih, data Anda berhasil tersimpan.", "success")
+            // console.log(d.status);
+            .then(
+          function() {
+            location.reload();
+          }, function() {
+            alert( "failed redirect" );
+          }
+        );
+          } else if (d.status === "not ok") {
+            swal("Error", "Data tidak ditemukan, tolong cek kembali pilihan anda.", "error");
+            // console.log(d.status);
+          } else {}
+      }
+    });
+  }
+}
+</script>
 <!-- Modal -->
   <div id="myModaladd" class="modal fade" role="dialog">
     <div class="modal-dialog">
@@ -17,24 +93,23 @@
         <!-- body modal -->
         <div class="modal-body">
           <div class="content">
-              <form action="<?php echo base_url()?>Schedule/do_insert" method="post">
                   <div class="row">
                       <div class="col-md-4">
                           <div class="form-group">
                               <label>Schedule ID</label>
-                              <input type="text" class="form-control border-input" readonly value="" name="schedule_id" placeholder="(autofill by system)" style="font-size: 0.5em;">
+                              <input type="text" id="schedule_id" class="form-control border-input" readonly value="" name="schedule_id" placeholder="(autofill by system)" style="font-size: 0.5em;">
                           </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Start Time</label>
-                            <input type="time" required class="form-control border-input" placeholder="start_time" name="start_time" value="">
+                            <input type="time" required id="start_time" class="form-control border-input" placeholder="start_time" name="start_time" value="">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Finish Time</label>
-                            <input type="time" required class="form-control border-input" placeholder="finish_time" name="finish_time" value="">
+                            <input type="time" required id="finish_time" class="form-control border-input" placeholder="finish_time" name="finish_time" value="">
                         </div>
                       </div>
                   </div>
@@ -43,19 +118,19 @@
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Day</label>
-                            <input type="text" readonly required class="form-control border-input" placeholder="day" name="day" value="Senin">
+                            <input type="text" readonly required id="day" class="form-control border-input" placeholder="day" name="day" value="Senin">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
-                            <label>Jam Awal</label>
-                            <input type="number" min="0" max="10" required class="form-control border-input" placeholder="jam ke- (awal)" name="jam mulai" value="">
+                            <label>Jam Mulai</label>
+                            <input type="number" min="0" max="10" required id="jam_mulai" class="form-control border-input" placeholder="jam ke- (awal)" name="jam_mulai" value="">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Jam Akhir</label>
-                            <input type="number" min="0" max="10" required class="form-control border-input" placeholder="jam ke- (akhir)" name="jam akhir" value="">
+                            <input type="number" min="0" max="10" required id="jam_akhir" class="form-control border-input" placeholder="jam ke- (akhir)" name="jam_akhir" value="">
                         </div>
                       </div>
                   </div>
@@ -63,7 +138,7 @@
                       <div class="col-md-5">
                         <div class="form-group">
                             <label>Guru</label>
-                            <select name="guru_id" class="form-control border-input select-guru">
+                            <select id="guru_id" name="guru_id" class="form-control border-input select-guru">
                                 <option value="">---</option>
                               <?php foreach ($data_guru as $value) { //print_r($value);?>
                                 <option value="<?php echo $value['guru_id']?>"><?php echo $value['guruname'] ?></option>
@@ -74,7 +149,7 @@
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Mapel</label>
-                            <select name="mapel_id" class="form-control border-input select-mapel">
+                            <select id="mapel_id" name="mapel_id" class="form-control border-input select-mapel">
                               <option value="">---</option>
                             <?php foreach ($data_mapel as $value) { //print_r($value);?>
                               <option value="<?php echo $value['mapel_id']?>"><?php echo $value['mapelname'] ?></option>
@@ -85,7 +160,7 @@
                       <div class="col-md-3">
                         <div class="form-group">
                             <label>Room</label>
-                            <select name="room_id" class="form-control border-input select-room">
+                            <select id="room_id" name="room_id" class="form-control border-input select-room">
                               <option value="">---</option>
                             <?php foreach ($data_room as $value) { //print_r($value);?>
                               <option value="<?php echo $value['room_id']?>"><?php echo $value['roomname'] ?></option>
@@ -98,7 +173,7 @@
                       <div class="col-md-12">
                           <div class="form-group">
                               <label>Note</label>
-                              <textarea name="note" class="form-control border-input"></textarea>
+                              <textarea id="note" class="form-control border-input" name="note"></textarea>
                           </div>
                       </div>
                   </div>
@@ -106,27 +181,26 @@
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Kelas</label>
-                            <input type="text" class="form-control border-input" readonly value="<?php echo $kelas;?>" name="kelas" placeholder="(autofill by system)">
+                            <input type="text" id="kelas" class="form-control border-input" readonly value="<?php echo $kelas;?>" name="kelas" placeholder="(autofill by system)">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Jurusan</label>
-                            <input type="text" class="form-control border-input" readonly value="<?php echo $jurusan;?>_<?php echo $jurusan_describe;?>" name="jurusan" placeholder="(autofill by system)">
+                            <input type="text" id="jurusan" class="form-control border-input" readonly value="<?php echo $jurusan;?>_<?php echo $jurusan_describe;?>" name="jurusan" placeholder="(autofill by system)">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                             <label>Subkelas</label>
-                            <input type="text" class="form-control border-input" readonly value="<?php echo $subkelas;?>" name="subkelas" placeholder="(autofill by system)">
+                            <input type="text" id="subkelas" class="form-control border-input" readonly value="<?php echo $subkelas;?>" name="subkelas" placeholder="(autofill by system)">
                         </div>
                       </div>
                   </div>
                   <div class="text-center">
-                      <button type="submit" class="btn btn-warning btn-fill btn-wd">Simpan Data</button>
+                      <button type="submit" class="btn btn-warning btn-fill btn-wd" onclick="submit_data()">Simpan Data</button>
                   </div>
                   <div class="clearfix"></div>
-              </form>
           </div>
         </div>
         <!-- footer modal -->
